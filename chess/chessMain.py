1297a0572a01195ec97412d748c1d5654f5d6a64
manousevens <manou.sevens@provil.be>
2021-03-10 18:18:29 +0100

    undo en begin van moves genereren

--- chess/chessMain.py
+++ chess/chessMain.py
@@ -22,6 +22,8 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = chessEngine.GameState()
+    validMoves = gs.getValidMoves()
+    moveMade = False
     loadImage()
     running = True
     sqSelected = ()
@@ -43,13 +45,18 @@ def main():
                 if len(playerClicks) == 2:
                     move = chessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    gs.makeMove(move)
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
                     sqSelected = ()
                     playerClicks = []
             elif e.type == p.KEYDOWN:
                 if e.key == p.K_z:
                     gs.undoMove()
-
+                    moveMade = True
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
